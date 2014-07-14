88a3a13587f610a1a6b9698b729309107f970613
pdion891 <pdion891@apache.org>
2014-07-13 20:26:37 -0400

    begin generate_api_keys

---
+++ resources/generate_api_keys.rb
@@ -0,0 +1,30 @@
+# Cookbook Name:: cloudstack
+# Resource:: generate_api_keys
+# Author:: Pierre-Luc Dion (<pdion@cloudops.com>)
+#
+# Copyright:: Copyright (c) 2014 CloudOps.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.require "system_vm_template"
+
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+actions :create
+
+default_action :create
+
+attribute :username       , :name_attribute => true, :kind_of => String
+attribute :url            , :kind_of => String, :default => "http://localhost:8080/client/api"
+attribute :password       , :kind_of => String, :default => "password"
+
+attr_accessor :exists
