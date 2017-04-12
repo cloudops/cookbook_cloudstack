a9c3d9ac37e4beca77b51c1a39c085265f20ac1a
Jake Plimack <jake.plimack@gmail.com>
2017-04-12 15:27:51 -0600

    quotes

--- providers/system_template.rb
+++ providers/system_template.rb
@@ -55,7 +55,7 @@ def load_current_resource
   else
     if db_exist?(@current_resource.db_host, @current_resource.db_user, @current_resource.db_password)
       if @current_resource.url.nil?
-        cmd = Mixlib::ShellOut.new("mysql -h #{@current_resource.db_host} --user=#{@current_resource.db_user} --password=#{@current_resource.db_password} --skip-column-names -U cloud -e 'select max(url) from cloud.vm_template where type = \"SYSTEM\" and hypervisor_type = \"#{@current_resource.hypervisor}\" and removed is null'")
+        cmd = Mixlib::ShellOut.new("mysql -h #{@current_resource.db_host} --user=#{@current_resource.db_user} --password='#{@current_resource.db_password}' --skip-column-names -U cloud -e 'select max(url) from cloud.vm_template where type = \"SYSTEM\" and hypervisor_type = \"#{@current_resource.hypervisor}\" and removed is null'")
         cmd.run_command
         cmd.error!
         @current_resource.url(cmd.stdout.chomp)
