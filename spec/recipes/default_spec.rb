4450e27acc3cd76473287535a1d2e718e6b8af50
Khosrow Moossavi <khos2ow@gmail.com>
2018-12-14 15:06:28 -0500

    Suppress chefspec warning message

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'cloudstack::management_server' do
   let(:chef_run) do
-    ChefSpec::ServerRunner.new.converge(described_recipe)
+    ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708').converge(described_recipe)
   end
 
   it 'installs cloudstack-management' do
